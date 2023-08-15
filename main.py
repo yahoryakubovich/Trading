36de5c14ce60b7c0542ac2c3f7080da80d988a3b
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-08-15 17:48:21 +0300

    Вложенные структуры, модели, декоратор

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, requests, status
+from fastapi.requests import Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ResponseValidationError
+from fastapi.responses import JSONResponse
+
+from models import *
 
 app = FastAPI(
     title="Trading App"
 )
 
+
+@app.exception_handler(ResponseValidationError)
+async def validation_except_handler(request: Request, exc: ResponseValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()})
+    )
 
 @app.get("/")
 def hello():
@@ -14,6 +28,9 @@ fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "trader", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01Т00:00:00", "type_degree": "expert"}
+    ]},
 ]
 
 
@@ -45,3 +62,15 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
+
+
+@app.post("/users")
+def add_users(users: List[User]):
+    fake_users.extend(users)
+    return {"status": 200, "data": fake_users}
